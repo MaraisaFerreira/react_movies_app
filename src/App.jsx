48feded96044dc79ac6e3b8ec402b5617013ed21
maraisaferreira <ferreira.maraisacristina@gmail.com>
2022-06-08 12:41:45 -0300

    card init

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import './App.css';
 import Header from './components/Header';
+import MovieCard from './components/MovieCard';
 
 function App() {
 	return (
 		<div className='App'>
 			<Header />
+			<div className='movies-container'>
+				<MovieCard />
+			</div>
 		</div>
 	);
 }
---
+++ src/components/MovieCard.jsx
@@ -0,0 +1,7 @@
+import '../styles/MovieCard.css';
+
+const MovieCard = () => {
+	return <div className='movie-card'>Card</div>;
+};
+
+export default MovieCard;
