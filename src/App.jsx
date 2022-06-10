ba49a10e88747eebeaac135f3eada57d6721b191
maraisaferreira <ferreira.maraisacristina@gmail.com>
2022-06-10 11:05:09 -0300

    fetch movies

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,25 @@
+import { useEffect, useState } from 'react';
 import './App.css';
 import Header from './components/Header';
 import MovieCard from './components/MovieCard';
 
+const API_URL =
+	'https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=3fd2be6f0c70a2a598f084ddfb75487c&page=1';
+
 function App() {
+	const [movies, setMovies] = useState([]);
+
+	useEffect(() => {
+		fetch(API_URL)
+			.then((resp) => resp.json())
+			.then((json) => setMovies(json.results));
+	}, []);
+
 	return (
 		<div className='App'>
 			<Header />
 			<div className='movies-box'>
-				<MovieCard />
+				<MovieCard list={movies} />
 			</div>
 		</div>
 	);
--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,28 +1,36 @@
 import '../styles/MovieCard.css';
 
-const MovieCard = () => {
-	return (
-		<div className='movie-card'>
+const BANNER_URL_BASE = 'https://image.tmdb.org/t/p/w1280';
+
+const MovieCard = ({ list }) => {
+	const movies = list.map((movie) => (
+		<div className='movie-card' key={movie.id}>
 			<img
-				src='https://images.unsplash.com/photo-1654358557454-4a78eb185e88?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80'
-				alt='Temp'
+				src={BANNER_URL_BASE + movie.poster_path}
+				alt={movie.title}
 				className='movie-banner'
 			/>
 			<div className='movie-info'>
-				<h3>Description</h3>
-				<span className='green'>9.1</span>
+				<h3>{movie.title}</h3>
+				<span
+					className={
+						movie.vote_average >= 7
+							? 'green'
+							: movie.vote_average >= 5
+							? 'orange'
+							: 'red'
+					}
+				>
+					{movie.vote_average}
+				</span>
 				<div className='overview'>
 					<h3>Overview</h3>
-					<p>
-						Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos beatae
-						distinctio eligendi non voluptas laborum nisi placeat quam
-						consequuntur quisquam, culpa, ea veritatis, facere ipsa? Et id culpa
-						natus quaerat!
-					</p>
+					<p>{movie.overview}</p>
 				</div>
 			</div>
 		</div>
-	);
+	));
+	return movies;
 };
 
 export default MovieCard;
